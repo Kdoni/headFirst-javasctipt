670b38a025ada68975e4da31c9b42dcfa4cfaffb
kdoni <chrisvdoni@gmail.com>
2022-06-10 17:01:07 +0300

    battleship logic: set.

--- chapter-02/battleship.js
+++ chapter-02/battleship.js
@@ -1,24 +1,30 @@
-const shipPOne = 3;
-const shipPTwo = 4;
-const shipPThree = 5;
+var randomLocation = Math.floor(Math.random()*5);
+var shipPOne = randomLocation;
+var shipPTwo = shipPOne +1;
+var shipPThree = shipPOne +2;
 var guess;
 var hit =0;
 var tries = 0;
-var shipSunk = false;
+var shipSunk;
 
-while (shipSunk ===false){
-    let guess = Number(window.prompt("Enter a number from 0 to 6: "));
-    if (guess === 3 || guess === 4 || guess === 5){
-        alert("It's a hit!");
+while (!shipSunk){
+    guess = window.prompt("Please select a number from 0 to 6");
+    if (guess<0 || guess>6){
+        alert("Invalid input, try again...");
+        continue;
+    }else{
         tries +=1;
-        hit +=1;
-        if (hit ===3){
-            break;
+        }if (guess ==shipPOne || guess == shipPTwo || guess == shipPThree){
+            alert("It's a HIT!");
+            hit+=1;
+            if(hit ==3){
+                alert("The ship has been sunken");
+                shipSunk;
+                break;
+            }
+        }else{
+            alert("It's a MISS...");
         }
-    }
-    else{
-        alert("It's a miss...Try again...");
-        tries +=1;
-    }}
-alert("You sunk the ship"  + "It took you " +tries +" tries.")
-
+}
+alert("It took you " + tries + " tries " + "to sink the ship.");
+alert("Accuracy: " + (hit/tries)* 100 +"%");
