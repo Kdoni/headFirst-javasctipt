9f22d153d29adbc2ef6d384cf47e238bfefe3c4d
kdoni <chrisvdoni@gmail.com>
2022-06-18 16:22:02 +0300

    validating user's input.

--- chapter-08/book_code.js
+++ chapter-08/book_code.js
@@ -18,11 +18,11 @@ var model = {
     numShips: 3,
     ships: [{
         locations: ["06","16","26"],
-        hits: ["hit","hit","hit"]},
+        hits: ["","",""]},
         {locations: ["24","34","44"],
-        hits: ["hit","hit",""]},
+        hits: ["","",""]},
         {locations: ["10","11","12"],
-        hits: ["hit","","hit"]}
+        hits: ["","",""]}
     ],
     shipsSunk: 0,
     shipLength: 3,
@@ -32,13 +32,18 @@ var model = {
             var index = ship.locations.indexOf(guess);
             if (index>=0){
                 ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
                 if (this.isSunk(ship)){
+                    view.displayMessage("A battleship has been sunk!");
                     this.shipsSunk++;
                 }
                 return true;
                 //we have a hit (-1 will be returned if the indexOf method does not find a match);
             }
         }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
         return false;
     },
     isSunk: function(ship){
@@ -49,6 +54,44 @@ var model = {
         }
         return true;
     }
+};
+
+var controller = {
+    guesses:0,
+    processGuess: function parseGuess(guess){
+        var alphabet = ["A","B","C","D","E","F","G"];
+        //We construct an array with the 7 letters of the alphabet to match the user's input: eg. g3.
+        if (guess===null || guess.length!==2){
+            //if the user's guess/input is null or the length is not 2 eg. b3(we accept only two strings).
+            alert("Oops, please enter a letter and a number on the board.");
+        }else{
+            var firstChar = guess.charAt(0);
+            //we create a variable which stores the first letter of the two letter input.
+            var row = alphabet.indexOf(firstChar);
+            //next we make a new variable row, which will be the return index of the match
+            //of the first instance of the first letter. eg. If the user entered g3, then
+            //the index that will be return is 6, since the letter g, matches the the 6th index
+            //of the alphabet array.
+            var column = guess.charAt(1);
+            //we create a new variable "column" which stores the second letter of the user's input.
+            if (isNaN(row) || isNaN(column)){
+                alert("Oops, that isn't on the board.");
+                //if any of the two characters is not a number, we alert an error message.
+            }else if (row<0 || row>=model.boardSize || column<0 || column>=model.boardSize){
+                // if any of the characters eg. 03 is less than 0, or bigger than the size of
+                // of the board, in this case seven, alert an error message. 
+                alert("Oops, that's off the board!");
+            }else {
+                return row + column;
+                //if all went well return the concatenated strings row + column.
+            }
+
+        }
+        return null;
+        //in case of error return null.
+    }
+        //update view and model
+    
 };
 
 // var ships = [{
@@ -80,3 +123,22 @@ locations = ["06", "16", "26"];
 // view.displayMiss("25");
 // view.displayHit("26");
 // view.displayMessage("Puri Piri Pi, Puri Iksha PI");
+
+model.fire("53");
+model.fire("06");
+model.fire("16");
+model.fire("26");
+model.fire("34");
+model.fire("24");
+model.fire("44");
+model.fire("12");
+model.fire("02");
+model.fire("11");
+model.fire("10"); 
+
+let test = controller.processGuess("A0");
+console.log(test);
+console.log(controller.processGuess("B6"));
+console.log(controller.processGuess("G3"));
+console.log(controller.processGuess("H0"));
+console.log(controller.processGuess("A7"));
