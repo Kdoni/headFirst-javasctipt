0d88e10ec7670403536ad8102f51b2168bd189cb
kdoni <chrisvdoni@gmail.com>
2022-06-18 12:44:43 +0300

    more progress.

--- chapter-08/book_code.js
+++ chapter-08/book_code.js
@@ -13,10 +13,70 @@ var view = {
     }
 };
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-view.displayMessage("Puri Piri Pi, Puri Iksha PI");
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    ships: [{
+        locations: ["06","16","26"],
+        hits: ["hit","hit","hit"]},
+        {locations: ["24","34","44"],
+        hits: ["hit","hit",""]},
+        {locations: ["10","11","12"],
+        hits: ["hit","","hit"]}
+    ],
+    shipsSunk: 0,
+    shipLength: 3,
+    fire: function(guess){
+        for (var i =0;i<this.numShips;i++){
+            var ship = this.ships[i];
+            var index = ship.locations.indexOf(guess);
+            if (index>=0){
+                ship.hits[index] = "hit";
+                if (this.isSunk(ship)){
+                    this.shipsSunk++;
+                }
+                return true;
+                //we have a hit (-1 will be returned if the indexOf method does not find a match);
+            }
+        }
+        return false;
+    },
+    isSunk: function(ship){
+        for (var i= 0;i<this.shipLength;i++){
+            if (ship.hits[i] !== "hit"){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// var ships = [{
+//     locations: ["06","16","26"],
+//     hits: ["hit","hit","hit"]},
+//     {locations: ["24","34","44"],
+//     hits: ["hit","hit",""]},
+//     {locations: ["10","11","12"],
+//     hits: ["hit","","hit"]}
+// ];
+
+guess ="16";
+locations = ["06", "16", "26"];
+
+// for (var i =0;i<this.numShips;i++){
+//     var ship = this.ships[i];
+//     locations = ship.locations;
+//     var index = locations.indexOf(guess);
+//     if (index>=0){
+//         //we have a hit (-1 will be returned if the indexOf method does not find a match);
+//     }
+// }
+    
+
+// view.displayMiss("00");
+// view.displayHit("34");
+// view.displayMiss("55");
+// view.displayHit("12");
+// view.displayMiss("25");
+// view.displayHit("26");
+// view.displayMessage("Puri Piri Pi, Puri Iksha PI");
